7ac37dee1ec0203ed5ed44362c80b62926bf1d80
Stepan Karpov <stepan.karpov@list.ru>
2025-02-20 22:23:38 +0300

    feat

--- solana-get-historical-data/playground/helius-api.sh
+++ solana-get-historical-data/playground/helius-api.sh
@@ -13,3 +13,11 @@ curl -L \
   --data '{"id":"1","jsonrpc":"2.0","method":"getSignaturesForAddress","params":["6p6xgHyF7AeE6TZkSmFsko444wqoP15icUSqi2jfGiPN", {"limit": 1000, "before": "51bSAVjC2MDJukD5tBx4Lqgw5UvKGT7s143qcqM35YVuHbFPcxvQqSyvAsep3QyxwfjTSoVwRkwA84FfrowBiUif"}]}'
 
 # result in previous.json
+
+curl -L \
+  --request POST \
+  --url 'https://mainnet.helius-rpc.com/?api-key=<paste-your-api-key>' \
+  --header 'Content-Type: application/json' \
+  --data '{"jsonrpc":"2.0","id":1,"method":"getTransaction","params":["4naZGV5Fg327oTG4YgFokLR7CCKkrNDZgjd6Bgpv6jZcWHQHGs8SruMNaLY7fjoKTNiPg5ZHimKQAEyUmniLQqaf","json"]}'
+
+# result in transaction_info.json
